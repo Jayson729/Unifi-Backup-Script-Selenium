bc6454b06d01f8b708d5a06fe995e90467578f93
Jayson729 <91502167+Jayson729@users.noreply.github.com>
2023-12-01 15:46:14 -0500

    Add files via upload

---
+++ example_sites.py
@@ -0,0 +1,7 @@
+from unifi_backup_settings import UnifiBackupSettings
+
+def get_unifi_backup_settings() -> set[UnifiBackupSettings]:
+    backup_settings = [
+        # insert UnifiBackupSettings here
+    ]
+    return backup_settings
