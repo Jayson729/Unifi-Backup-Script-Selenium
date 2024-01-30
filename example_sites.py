669cdd05c3b9ae5fa0444915537ad27de987b71a
Jayson Willey <91502167+Jayson729@users.noreply.github.com>
2024-01-30 08:58:49 -0500

    changed example_sites.py, added .gitignore

--- example_sites.py
+++ example_sites.py
@@ -2,6 +2,7 @@ from unifi_backup_settings import UnifiBackupSettings
 
 def get_unifi_backup_settings() -> set[UnifiBackupSettings]:
     backup_settings = [
+        UnifiBackupSettings('Example', '000000000000000000000000000000000000000000000000000000000000:000000000'),
         # insert UnifiBackupSettings here
     ]
     return backup_settings
